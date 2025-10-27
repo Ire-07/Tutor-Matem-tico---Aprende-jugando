f8ce878f29cd831185e761e605ba6df57d2934d1
Irene <irevm07@gmail.com>
2025-10-27 15:58:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import streamlit as st
 from PIL import Image
+import os
 
-st.title("Tutor Matemático - Aprende Jungando en ControlMax")
-
-# Mostrar imagen
-img = Image.open("recursos\logo.png")
+BASE_DIR = os.path.dirname(__file__)
+img_path = os.path.join(BASE_DIR, "recursos", "logo.png")
+img = Image.open(img_path)
 st.image(img, width=150)
 
-# Interacción con el usuario
-num = st.number_input("Ingresa un número", min_value=1, max_value=100)
-st.write(f"El número que ingresaste es {num}")
